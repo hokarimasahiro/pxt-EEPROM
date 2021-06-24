1ff70c37acc8f71efee2b83aa744d3441870c68f
hokarimasahiro <42521359+hokarimasahiro@users.noreply.github.com>
2021-06-24 17:18:05 +0900

    更新 eeprom.ts

--- eeprom.ts
+++ eeprom.ts
@@ -155,14 +155,14 @@ namespace EEPROM {
         for(let i=0;i<dat.length;i++){
             buf[(i % 256) + 2] = dat.charCodeAt(i);
             if (((addr + i) % 256) == 255){
+serial.writeLine("1 " + buf[0] + " " + buf[1]);
                 pins.i2cWriteBuffer(address, buf);
-serial.writeLine("" + buf[0] + " " + buf[1]);
                 buf[0]++;
                 buf[1] = 0;
-serial.writeLine("" + buf[0] + " " + buf[1]);
             }
         }
         buf[(dat.length % 256) + 2] = 0x00;
+serial.writeLine("2 " + buf[0] + " " + buf[1]);
         pins.i2cWriteBuffer(address, buf)
     }
 
